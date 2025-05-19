50d00df7b16e7bde0e6f51833ebeb66b993be6ff
emperatorLeo <leo_svxy@hotmail.com>
2025-05-19 13:30:37 -0500

    fixing git ignore

---
+++ app/src/test/java/com/example/pokedexapp/TestViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.pokedexapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TestViewModel {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
